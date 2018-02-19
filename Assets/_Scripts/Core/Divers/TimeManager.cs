197974ca56aa262291d24200266d8614d8cd41e6
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-02-19 14:34:55 +0100

    new configuration level design

---
+++ Assets/_Scripts/Core/Divers/TimeManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+/// <summary>
+/// TimeManager Description
+/// </summary>
+public class TimeManager : MonoBehaviour
+{
+    #region Attributes
+    [FoldoutGroup("GamePlay"), Tooltip("slowDonwFactor"), SerializeField]
+    private float slowDonwFactor = 0.05f;             //le type de ball (bleu, red...)
+    [FoldoutGroup("GamePlay"), Tooltip("slowDonwFactor"), SerializeField]
+    private float slowDonwLenght = 2f;             //le type de ball (bleu, red...)
+
+    #endregion
+
+    #region Initialization
+
+    private void Start()
+    {
+		// Start function
+    }
+    #endregion
+
+    #region Core
+    private void DoSlowMothion()
+    {
+        Time.timeScale = slowDonwFactor;
+    }
+    #endregion
+
+    #region Unity ending functions
+
+	#endregion
+}
